f7dff57872464e46afd5572d7e78ce4d019ea2dd
Alex Hilson <alexhilsonwork@googlemail.com>
2015-12-14 23:33:32 +0000

    Added ability to request the orderbook for a Stock.

--- stockfighter/stock.py
+++ stockfighter/stock.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+import requests
+
+
+VENUES_API = "https://api.stockfighter.io/ob/api/venues"
+
 
 class Stock(object):
 
@@ -7,3 +12,9 @@ class Stock(object):
         self.venue = venue
         self.name = name
         self.symbol = symbol
+
+    def orderbook(self):
+        return requests.get(
+            "{}/{}/stocks/{}"
+            .format(VENUES_API, self.venue, self.symbol)
+        ).json()
--- tests/test_stock.py
+++ tests/test_stock.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import unittest
+import pkg_resources
+import json
 
 from stockfighter.stock import Stock
 
@@ -18,3 +20,8 @@ class TestExistingStock(unittest.TestCase):
         self.assertEquals(self.stock.venue, self.venue)
         self.assertEquals(self.stock.name, self.name)
         self.assertEquals(self.stock.symbol, self.symbol)
+
+    # Example stock orderbook is dynamic
+    def test_orderbook(self):
+        orderbook = self.stock.orderbook()
+        self.assertEqual(orderbook["venue"], self.venue)
